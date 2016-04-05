a707e53ee471ac93b608be861bcf2f66046d6125
Brandon DeRosier <bdero@mit.edu>
2016-04-05 13:25:40 -0400

    Simplify deploy command for config

--- edx-sandbox-deploy.sh
+++ edx-sandbox-deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd edx-ansible/playbooks/; ansible-playbook ./edx_sandbox.yml -i "micromasters.d.mitx.mit.edu," -u ubuntu --private-key ../../secrets/keys/mitx-devops --tags deploy --extra-vars "@../../environments/edx-sandbox.yml"
+ansible-playbook edx-ansible/playbooks/edx_sandbox.yml -i "micromasters.d.mitx.mit.edu," --tags deploy --extra-vars "@../../environments/edx-sandbox.yml"
