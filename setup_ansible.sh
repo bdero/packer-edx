dc332d83579eb025002edf598ced1c0faa1ae6fe
Brandon DeRosier <bdero@mit.edu>
2016-04-08 08:24:41 -0400

    Add setup script for ansible-local

--- setup_ansible.sh
+++ setup_ansible.sh
@@ -1,8 +1,20 @@
-# Install pip
-sudo apt-get --yes update
-sudo apt-get --yes dist-upgrade
-sudo apt-get --yes install python python-pip
+#!/bin/sh -x
 
-# Install the requirements
-sudo pip install --yes -r ./edx-ansible/pre-requirements.txt
-sudo pip install --yes -r ./edx-ansible/requirements.txt
+# Install python
+
+# Running `apt-get update` this early results in git's dependency chain
+# breaking (liberror-perl), so don't.
+#sudo apt-get --yes update
+
+sudo apt-get --yes install python2.7 git libmysqlclient-dev --upgrade
+
+# Install setuptools
+curl -0 http://peak.telecommunity.com/dist/ez_setup.py > /tmp/ez_setup.py
+sudo python /tmp/ez_setup.py
+
+# Install pip
+sudo easy_install pip
+
+# Install the requirements (includes the correct version of Ansible)
+sudo pip --no-input install -r pre-requirements.txt
+sudo pip --no-input install -r requirements.txt
